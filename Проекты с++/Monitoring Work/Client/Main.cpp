e4a892d68fd11970489b7b29bac4f23e050967cc
N_eFFe_X <grushnikov_k@mail.ru>
2024-07-01 20:12:46 +0300

    delete input ip

--- Проекты с++/Monitoring Work/Client/Main.cpp
+++ Проекты с++/Monitoring Work/Client/Main.cpp
@@ -32,8 +32,6 @@ int main(int argc, char* argv[])
     uint16_t port = rand() % 20000;
     uint16_t server_port = 56091;
     std::string ip = "127.0.0.1";
-    std::cout << "E: ";
-    std::cin >> ip;
 
     ClientServer c_server(port);
 
